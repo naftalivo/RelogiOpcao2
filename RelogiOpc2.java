057d54c09bdb3ba779b1e916004e76f29d4df9bc
Ivo Naftal <31237771+naftalivo@users.noreply.github.com>
2019-11-27 17:12:01 +0200

    Add files via upload

--- RelogiOpc2.java
+++ RelogiOpc2.java
@@ -9,6 +9,7 @@ import java.time.LocalTime;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Keyboard;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
 import static org.lwjgl.opengl.GL11.*;
@@ -64,20 +65,28 @@ public class RelogiOpc2 {
             }
 
             DesenhoRelogio();
+
+            //Acertar hora
+//            if (Keyboard.isKeyDown(Keyboard.KEY_UP) && !Keyboard.isKeyDown(Keyboard.KEY_DOWN)) {
+//                HoursAjust += 0.1;
+//            }
+//            if (Keyboard.isKeyDown(Keyboard.KEY_DOWN) && !Keyboard.isKeyDown(Keyboard.KEY_UP)) {
+//                MinutAjust += 1;
+//            }
             
             /*H - Metodo para aumetar o Tamanho do ponteir.. das Horas*/
             if (Keyboard.isKeyDown(Keyboard.KEY_H)) {
-                PonteiroHoras += 15;
+                PonteiroHoras += 25;
             }
             
             /*M - Metodo para aumetar o Tamanho do ponteir.. dos Minutos*/
             if (Keyboard.isKeyDown(Keyboard.KEY_M)) {
-                PonteiroMinutos += 15;
+                PonteiroMinutos += 25;
             }
             
             /*S - Metodo para aumetar o Tamanho do ponteir.. dss Segundos*/
             if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
-                PonteiroSegundos += 15;
+                PonteiroSegundos += 25;
             }
             
             //Metodo para sair do sistema com a tecla - SHIFT
@@ -111,6 +120,70 @@ public class RelogiOpc2 {
             y = posY + r * Math.sin(2 * Math.PI * i / segments);
             glVertex2d(x, y);
         }
-        glEnd();      
+        glEnd();
+
+        // 60 - Pontos dos Segundos e Minutos no Relogio
+        glColor3d(0.0, 0.0, 0.0);
+        for (int i = 0; i < 60; i++) {
+            glBegin(GL_POINTS);
+            x = posX + r * Math.cos(2 * Math.PI * i / 60); /*Pontos ao redor do circulo*/
+            y = posY + r * Math.sin(2 * Math.PI * i / 60);
+            
+            glVertex2d(x, y);
+            x = posX + 0.040 * r * Math.cos(2 * Math.PI * i / 60); /*Pontos dentro do circulo*/
+            y = posY + 0.045 * r * Math.sin(2 * Math.PI * i / 60);
+            glVertex2d(x, y);
+            glEnd();
+        }
+
+        // 12 Pontos das Horas no Relogio
+        glColor3d(0.0, 0.0, 0.0);
+        for (int i = 0; i < 12; i++) {
+            glBegin(GL_LINE_LOOP);
+            x = posX + r * Math.cos(2 * Math.PI * i / 12);
+            y = posY + r * Math.sin(2 * Math.PI * i / 12);
+            glVertex2d(x, y);
+            x = posX + 0.9 * r * Math.cos(2 * Math.PI * i / 12);
+            y = posY + 0.9 * r * Math.sin(2 * Math.PI * i / 12);
+            glVertex2d(x, y);
+            glEnd();
+        }
+        
+        /*Pegando as Horas do sistema*/
+        final LocalTime time = LocalTime.now();
+        double hora = time.getHour() + time.getMinute() / 60.0 + time.getSecond() / 3600.0 + HoursAjust;
+        double minuto = time.getMinute() + time.getSecond() / 60.0 + MinutAjust;
+        double segundo = time.getSecond();
+
+        // Ponteiro das Horas
+        x = posX + 0.5 * r * Math.cos(5 * Math.PI / 2 - 2 * Math.PI * hora / 12);
+        y = posY + 0.5 * r * Math.sin(5 * Math.PI / 2 - 2 * Math.PI * hora / 12);
+        glLineWidth(PonteiroHoras);
+        glBegin(GL_LINE_LOOP);
+        glColor3d(1.0f, 0.5f, 0.0f);
+        glVertex2d(posX, posY);
+        glVertex2d(x, y);
+        glEnd();
+        
+        // Ponteiro dos Minutos
+        x = posX + 0.85 * r * Math.cos(5 * Math.PI / 2 - 2 * Math.PI * minuto / 60);
+        y = posY + 0.85 * r * Math.sin(5 * Math.PI / 2 - 2 * Math.PI * minuto / 60);
+        glLineWidth(PonteiroMinutos);
+        glBegin(GL_LINE_LOOP);
+        glColor3d(0.1, 0.75, 0.5);
+        glVertex2d(posX, posY);
+        glVertex2d(x, y);
+        glEnd();
+        
+        // Ponteiro dos Segundos
+        x = posX + 0.9 * r * Math.cos(5 * Math.PI / 2 - 2 * Math.PI * segundo / 60);
+        y = posY + 0.9 * r * Math.sin(5 * Math.PI / 2 - 2 * Math.PI * segundo / 60);
+        glLineWidth(PonteiroSegundos);
+        glBegin(GL_LINE_LOOP);
+        glColor3d(1.0, 0.9, 0.0);
+        glVertex2d(posX, posY);
+        glVertex2d(x, y);
+        glEnd();
+    }
 
 }
